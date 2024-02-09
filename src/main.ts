b4411c18dfec2b08e4e0550a18f65fadbe188083
Emilian Adrian Hossu <emil.hossu@gmail.com>
2024-02-09 11:00:25 +0100

    small changes

--- src/main.ts
+++ src/main.ts
@@ -24,20 +24,20 @@ function createWindow() {
 app.whenReady().then(() => {
     createWindow();
 
-    app.on("activate", function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+    // app.on("activate", function () {
+    //     // On macOS it's common to re-create a window in the app when the
+    //     // dock icon is clicked and there are no other windows open.
+    //     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    // });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
+    // if (process.platform !== "darwin") {
         app.quit();
-    }
+    // }
 });
 
 // In this file you can include the rest of your app"s specific main process
