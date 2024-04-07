6540fd62afb1ea881c2f1be2dec88c7600bd0ee1
Emilian Adrian Hossu <emil.hossu@gmail.com>
2024-04-07 21:40:23 +0300

    instrument 3 - WIP

--- src/libraries/build_templates.ts
+++ src/libraries/build_templates.ts
@@ -4,17 +4,21 @@ import { Eta } from "eta"
 import { writeFileSync } from "original-fs";
 import * as path from "path";
 
-import * as one_en from "../templates/4_UZ_ChildDI_Material_Resources_en.json";
-import * as one_ru from "../templates/4_UZ_ChildDI_Material_Resources_ru.json";
-import * as one_uz from "../templates/4_UZ_ChildDI_Material_Resources_uz.json";
+import * as one_en from "../templates/3_UZ_ChildDI_Staff_Registry_en.json";
+import * as one_ru from "../templates/3_UZ_ChildDI_Staff_Registry_ru.json";
+import * as one_uz from "../templates/3_UZ_ChildDI_Staff_Registry_uz.json";
 
 export default function build_templates() {
 
     const etaObj = new Eta({ views: path.join(__dirname, "../../src/templates") })
+    
+    writeFileSync(path.join(__dirname, "../../src/templates/3_UZ_ChildDI_Staff_Registry_en.html"), etaObj.render("3_UZ_ChildDI_Staff_Registry.eta", one_en))
+    writeFileSync(path.join(__dirname, "../../src/templates/3_UZ_ChildDI_Staff_Registry_ru.html"), etaObj.render("3_UZ_ChildDI_Staff_Registry.eta", one_ru))
+    writeFileSync(path.join(__dirname, "../../src/templates/3_UZ_ChildDI_Staff_Registry_uz.html"), etaObj.render("3_UZ_ChildDI_Staff_Registry.eta", one_uz))
 
-    writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_en.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_en))
-    writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_ru.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_ru))
-    writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_uz.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_uz))
+    // writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_en.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_en))
+    // writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_ru.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_ru))
+    // writeFileSync(path.join(__dirname, "../../src/templates/4_UZ_ChildDI_Material_Resources_uz.html"), etaObj.render("4_UZ_ChildDI_Material_Resources.eta", one_uz))
 
     // import * as one_en from "./templates/1_UZ_ChildDI_Children_in_the_child_care_system_en.json";
     // import * as one_ru from "./templates/1_UZ_ChildDI_Children_in_the_child_care_system_ru.json";
