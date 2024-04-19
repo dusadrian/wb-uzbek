3eb29777a08754b7d9fa25a4506ff31ee57696ff
Emilian Adrian Hossu <emil.hossu@gmail.com>
2024-04-19 09:06:08 +0300

    instrument 5 - WIP

--- src/pages/instruments/05_yplcs_instrument.ts
+++ src/pages/instruments/05_yplcs_instrument.ts
@@ -1,5 +1,5 @@
 import { ipcRenderer } from "electron";
-import { questions, questionOrder } from "./03_csr_variables";
+import { questions, questionOrder } from "./05_yplcs_variables";
 import instrument from "../../libraries/instrument";
 import { QuestionObjectType, SaveInstrumentType } from "../../libraries/interfaces";
 
@@ -35,8 +35,8 @@ export const instrument5 = {
             flatpickrConfig.locale = Russian;
         }
 
-        flatpickr((<HTMLInputElement>document.getElementById('e2')), flatpickrConfig);
-        flatpickr((<HTMLInputElement>document.getElementById('e7')), flatpickrConfig);
+        // flatpickr((<HTMLInputElement>document.getElementById('e2')), flatpickrConfig);
+        // flatpickr((<HTMLInputElement>document.getElementById('e7')), flatpickrConfig);
 
         ipcRenderer.on("instrumentDataReady", (_event, args) => {
 
@@ -55,68 +55,26 @@ export const instrument5 = {
                 }
 
             } else {
-                const q1 = (<HTMLInputElement>document.getElementById('q1'));
+                const data = (<HTMLInputElement>document.getElementById('data'));
                 // two digit day & month
-                q1.value = new Date().getDate().toString().padStart(2, '0') + "/" + (new Date().getMonth() + 1).toString().padStart(2, '0') + "/" + new Date().getFullYear().toString();
+                data.value = new Date().getDate().toString().padStart(2, '0') + "/" + (new Date().getMonth() + 1).toString().padStart(2, '0') + "/" + new Date().getFullYear().toString();
 
                 if (args.userData) {
                     // set default values for user
-                    const q2 = (<HTMLInputElement>document.getElementById('q2'));
-                    q2.value = args.userData.first_name + " " + args.userData.patronymics + " " + args.userData.last_name;
-                    const q3 = (<HTMLInputElement>document.getElementById('q3'));
-                    q3.value = args.userData.position;
-                    const q4 = (<HTMLInputElement>document.getElementById('q4'));
-                    q4.value = args.userData.profession;
-                    const q5 = (<HTMLInputElement>document.getElementById('q5'));
-                    q5.value = args.userData.phone;
-                    const q6 = (<HTMLInputElement>document.getElementById('q6'));
-                    q6.value = args.userData.email;
-                }
-
-                if (args.institutionData) {
-
-                    // set default values for institution
-                    const i1 = (<HTMLInputElement>document.getElementById('i1'));
-                    i1.value = args.institutionData.name;
-                    const i2 = (<HTMLInputElement>document.getElementById('i2'));
-                    i2.value = args.institutionData.code;
-                    const i3 = (<HTMLInputElement>document.getElementById('i3'));
-                    i3.value = args.institutionData.address;
-                    const i4 = (<HTMLInputElement>document.getElementById('i4'));
-                    i4.value = args.institutionData.atuCode;
-                    const i4a = (<HTMLInputElement>document.getElementById('i4a'));
-                    const i4b = (<HTMLInputElement>document.getElementById('i4b'));
-                    const i4c = (<HTMLInputElement>document.getElementById('i4c'));
-
-                    const regions = Object.keys(administrative);
-                    if (regions.indexOf(args.institutionData.region) >= 0) {
-
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                        i4a.value = (administrative[args.institutionData.region] as any)[lang];
-                        const regdist = administrative[i4a.value].districts;
-                        const districts = Object.keys(regdist);
-
-                        if (districts.indexOf(args.institutionData.district) >= 0) {
-                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                            i4b.value = (regdist[args.institutionData.district] as any)[lang];
-
-                            const regdisatu = administrative[i4a.value].districts[i4b.value].settlements;
-
-                            if (regdisatu) {
-                                const settlements = Object.keys(regdisatu);
-                                if (settlements.indexOf(args.institutionData.settlement) >= 0) {
-                                    i4c.value = (regdisatu[args.institutionData.settlement] as { [key: string]: string })[lang];
-                                }
-                            }
-                        }
-                    }
-
-                    // TODO -- To be updated -- Type of settlement
-                    const i4d = (<HTMLInputElement>document.getElementById('i4d'));
-                    i4d.value = args.institutionData.district;
-                    // Type of institution
-                    const i5 = (<HTMLInputElement>document.getElementById('i5'));
-                    i5.value = args.institutionData.type;
+                    const omr1 = (<HTMLInputElement>document.getElementById('omr1'));
+                    omr1.value = args.userData.first_name;
+                    const omr2 = (<HTMLInputElement>document.getElementById('omr2'));
+                    omr2.value = args.userData.patronymics;
+                    const omr3 = (<HTMLInputElement>document.getElementById('omr3'));
+                    omr3.value = args.userData.last_name;
+                    const omr4 = (<HTMLInputElement>document.getElementById('omr4'));
+                    omr4.value = args.userData.position;
+                    const omr5 = (<HTMLInputElement>document.getElementById('omr5'));
+                    omr5.value = args.userData.profession;
+                    const omr6 = (<HTMLInputElement>document.getElementById('omr6'));
+                    omr6.value = args.userData.phone;
+                    const omr7 = (<HTMLInputElement>document.getElementById('omr7'));
+                    omr7.value = args.userData.email;
                 }
 
             }
