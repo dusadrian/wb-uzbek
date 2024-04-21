3dc857cdba3be589cb618568d2625af26f50430f
Adrian Dușa <dusa.adrian@gmail.com>
2024-04-21 18:57:55 +0300

    activat YPLCS

--- src/pages/cityCollector/01_dashboard.ts
+++ src/pages/cityCollector/01_dashboard.ts
@@ -23,13 +23,13 @@ export const cityCollector = {
         }
 
         // - Young people who left care system
-        // const viewEdit_FTCH = (<HTMLButtonElement>document.getElementById('viewEdit_FTCH'));
-        // if (viewEdit_FTCH !== null) {
-        //     viewEdit_FTCH.addEventListener('click', () => {
-        //         ipcRenderer.send('changeWindow', {
-        //             'name': 'ftch',
-        //         });
-        //     });
-        // }
+        const viewEdit_YPLCS = (<HTMLButtonElement>document.getElementById('viewEdit_YPLCS'));
+        if (viewEdit_YPLCS !== null) {
+            viewEdit_YPLCS.addEventListener('click', () => {
+                ipcRenderer.send('changeWindow', {
+                    'name': 'yplcs',
+                });
+            });
+        }
     }
 }
