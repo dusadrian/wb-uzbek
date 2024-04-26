7cc71fc180c83f07545d8deff83b6bc5e507d080
Emilian Adrian Hossu <emil.hossu@gmail.com>
2024-04-26 16:21:05 +0300

    user migration update

--- src/database/migrations.sql
+++ src/database/migrations.sql
@@ -18,19 +18,9 @@ CREATE TABLE users (
     profession VARCHAR NULL,
     phone VARCHAR NULL,
     email VARCHAR NULL,
-    role_name VARCHAR NULL,
     region_code VARCHAR NULL,
     role_code VARCHAR NULL,
     service_type_code VARCHAR NULL,
-    q1 VARCHAR NULL,
-    q2 VARCHAR NULL,
-    q3 VARCHAR NULL,
-    q4 VARCHAR NULL,
-    q5 VARCHAR NULL,
-    q5a VARCHAR NULL,
-    q6 VARCHAR NULL,
-    q7 VARCHAR NULL,
-    q8 VARCHAR NULL,
     PRIMARY KEY (id)
 );
 
@@ -234,20 +224,4 @@ CREATE TABLE values_tqyp (
     value VARCHAR NOT NULL,
     PRIMARY KEY (instrument_id, variable)
 );
-
-INSERT INTO
-    users (username,password,institution_code,institution_name,name,patronymics,surname,job_title,profession,phone,email,role_name,region_code,role_code,service_type_code,q1,q2,q3,q4,q6,q5a,q5,q7,q8)
-VALUES
-    ('national', 'national', '', '', '', '', '', '', '', '', '', '', '1703', '100', '', '', '', '', '', '', '', '', '', ''),
-    ('regional', 'regional', '', '', '', '', '', '', '', '', '', '', '1703', '10', '', '', '', '', '', '', '', '', '', ''),
-    ('evaluator', 'evaluator', '', '', '', '', '', '', '', '', '', '', '1703', '5', '', '', '', '', '', '', '', '', '', ''),
-    ('admin_specialit', 'admin_specialit', '', '', '', '', '', '', '', '', '', '', '1703', '4', '1', '', '', '', '', '', '', '', '', ''),
-    ('hr_specialit', 'hr_specialit', '', '', '', '', '', '', '', '', '', '', '1703', '3', '1', '', '', '', '', '', '', '', '', ''),
-    ('childcare', 'childcare', '', '', '', '', '', '', '', '', '', '', '1703', '2', '1', '', '', '', '', '', '', '', '', ''),
-    ('boarding', 'boarding', '', '', '', '', '', '', '', '', '', '', '1703', '2', '4', '', '', '', '', '', '', '', '', ''),
-    ('inson', 'inson', '', '', '', '', '', '', '', '', '', '', '1703', '2', '9', '', '', '', '', '', '', '', '', ''),
-    ('coordinator_cd', 'coordinator_cd', '', '', '', '', '', '', '', '', '', '', '1703', '1', '1', '', '', '', '', '', '', '', '', ''),
-    ('coordinator_boarding', 'coordinator_boarding', '', '', '', '', '', '', '', '', '', '', '1703', '1', '4', '', '', '', '', '', '', '', '', ''),
-    ('coordinator_inson', 'coordinator_inson', '', '', '', '', '', '', '', '', '', '', '1703', '1', '9', '', '', '', '', '', '', '', '', '');
-
 
