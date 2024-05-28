78708b75e275a783b334c4d14851fc96014ac252
Emilian-Adrian Hossu <emil.hossu@gmail.com>
2024-05-28 14:27:12 +0300

    update ftch edit/delete button bind

--- src/pages/instruments/07_ftch.ts
+++ src/pages/instruments/07_ftch.ts
@@ -11,6 +11,26 @@ const dt = window.require('datatables.net-dt');
 dt(window, $);
 
 let institutionCode: string;
+const boundFunctionsEdit = new Map();
+const boundFunctionsDelete = new Map();
+const callbackEditDelete = () => {
+    document.querySelectorAll('.editButton').forEach(item => {
+        const boundFunctionToRemove = boundFunctionsEdit.has(item);
+        if (!boundFunctionToRemove) {
+            const boundFunction = editItem.bind(this, (<HTMLButtonElement>item).dataset.myid);
+            item.addEventListener('click', boundFunction);
+            boundFunctionsEdit.set(item, boundFunction);
+        }
+    });
+    document.querySelectorAll('.deleteButton').forEach(item => {
+        const boundFunctionToRemove = boundFunctionsDelete.has(item);
+        if (!boundFunctionToRemove) {
+            const boundFunction = deleteItem.bind(this, (<HTMLButtonElement>item).dataset.myid);
+            item.addEventListener('click', boundFunction);
+            boundFunctionsDelete.set(item, boundFunction);
+        }
+    });
+}
 
 export const ftch = {
     init: async () => {
@@ -47,7 +67,8 @@ export const ftch = {
             "createdRow": function (row: HTMLTableRowElement) {
                 // $(row).addClass('text-center');
                 $(row).addClass('align-middle');
-            }
+            },
+            drawCallback: callbackEditDelete,
         });
 
         ipcRenderer.on('ftch', (event, ftch) => {
@@ -94,11 +115,11 @@ const fillTable = (table: any, ftch: Array<any>) => {
         buttons.appendChild(buttonDelete);
 
         const status = document.createElement('div');
-        status.classList.add('flex'); 
-        status.classList.add('justify-center'); 
-        if(item.status == 'partial') { 
+        status.classList.add('flex');
+        status.classList.add('justify-center');
+        if (item.status == 'partial') {
             status.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-circle" class="w-4 h-4 text-yellow-500" role="img" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path></svg>'
-        } else if(item.status == 'completed') {
+        } else if (item.status == 'completed') {
             status.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 text-green-500" role="img" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>';
         }
 
@@ -112,12 +133,7 @@ const fillTable = (table: any, ftch: Array<any>) => {
         ]).draw();
     });
 
-    document.querySelectorAll('.editButton').forEach(item => {
-        item.addEventListener('click', editItem.bind(this, (<HTMLButtonElement>item).dataset.myid));
-    });
-    document.querySelectorAll('.deleteButton').forEach(item => {
-        item.addEventListener('click', deleteItem.bind(this, (<HTMLButtonElement>item).dataset.myid));
-    });
+    callbackEditDelete();
 };
 
 
