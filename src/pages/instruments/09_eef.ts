2b30946837b79e1f8e0afccc3de918b77bf45f3b
Emilian-Adrian Hossu <emil.hossu@gmail.com>
2024-05-28 14:30:41 +0300

    update eef edit/delete button bind

--- src/pages/instruments/09_eef.ts
+++ src/pages/instruments/09_eef.ts
@@ -9,6 +9,26 @@ const $ = window.require('jquery');
 const dt = window.require('datatables.net-dt');
 dt(window, $);
 
+const boundFunctionsEdit = new Map();
+const boundFunctionsDelete = new Map();
+const callbackEditDelete = () => {
+    document.querySelectorAll('.editButton').forEach(item => {
+        const boundFunctionToRemove = boundFunctionsEdit.has(item);
+        if (!boundFunctionToRemove) {
+            const boundFunction = editItem.bind(this, (<HTMLButtonElement>item).dataset.myid);
+            item.addEventListener('click', boundFunction);
+            boundFunctionsEdit.set(item, boundFunction);
+        }
+    });
+    document.querySelectorAll('.deleteButton').forEach(item => {
+        const boundFunctionToRemove = boundFunctionsDelete.has(item);
+        if (!boundFunctionToRemove) {
+            const boundFunction = deleteItem.bind(this, (<HTMLButtonElement>item).dataset.myid);
+            item.addEventListener('click', boundFunction);
+            boundFunctionsDelete.set(item, boundFunction);
+        }
+    });
+}
 
 export const eef = {
     init: async () => {
@@ -38,7 +58,8 @@ export const eef = {
             },
             "createdRow": function (row: HTMLTableRowElement) {
                 $(row).addClass('align-middle');
-            }
+            },
+            drawCallback: callbackEditDelete,
         });
 
         ipcRenderer.on('eef', (event, eef) => {
@@ -100,12 +121,8 @@ const fillTable = (table: any, items: Array<any>) => {
         ]).draw();
 
     });
-    document.querySelectorAll('.editButton').forEach(item => {
-        item.addEventListener('click', editItem.bind(this, (<HTMLButtonElement>item).dataset.myid));
-    });
-    document.querySelectorAll('.deleteButton').forEach(item => {
-        item.addEventListener('click', deleteItem.bind(this, (<HTMLButtonElement>item).dataset.myid));
-    });
+
+    callbackEditDelete();
 };
 
 
